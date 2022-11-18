e5e141ea42a2c6a285e9e2f4926a655ac7eccb46
nyirurugo omar <nyirurugoomar@gmail.com>
2022-11-18 18:16:30 +0200

    Our partners section done

--- src/components/Ourimpact.jsx
+++ src/components/Ourimpact.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import CountUp from 'react-countup';
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import ProductionQuantityLimitsOutlinedIcon from '@mui/icons-material/ProductionQuantityLimitsOutlined';
+import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
 
 
 function Ourimpact() {
   return (
     <>
 <h1 className='text-center text-orange font-bold text-6xl'>Our Impact</h1>
-<div className='max-w-[1240px] mx-auto mt-40 mb-80  '>
+<div className='max-w-[1240px] mx-auto mt-10 mb-20  '>
     <div className='grid grid-cols-1 lg:grid-cols-3  gap-x-8 gap-y-16  pt-8 sm:pt-20  '>
       <div className=' bg-white px-20 shadow-2xl py-8 rounded-xl  '>
         
@@ -16,11 +17,11 @@ function Ourimpact() {
     <h1  className='text-orange font-bold text-5xl text-center'>
     
     <div className=' text-black mr-60 flex '>
-        <PeopleAltIcon fontSize='70rem'/>
+        <PersonOutlinedIcon fontSize='70rem'/>
       </div> 
      +<CountUp
             start={0}
-            end={500}
+            end={1000}
             duration={9.20}
             /> 
             {" "}
@@ -33,7 +34,7 @@ function Ourimpact() {
     <h1  className='text-orange font-bold text-5xl text-center'>
     
     <div className=' text-black mr-60 flex '>
-        <PeopleAltIcon fontSize='20rem'/>
+        <ProductionQuantityLimitsOutlinedIcon fontSize='20rem'/>
       </div> 
      +<CountUp
             start={0}
@@ -49,11 +50,11 @@ function Ourimpact() {
     <h1  className=' text-orange font-bold text-5xl text-center'>
     
     <div className=' text-black mr-60 flex '>
-        <PeopleAltIcon fontSize='20rem'/>
+        <PublicOutlinedIcon fontSize='20rem'/>
     </div> 
      +<CountUp
             start={0}
-            end={500}
+            end={80}
             duration={9.20}
             /> 
             {" "}
--- src/components/Ourpartners.jsx
+++ src/components/Ourpartners.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-
+import google from '../assets/google.png'
+import dropbox from '../assets/dropbox.png'
+import shopify from '../assets/shopify.png'
+import slack from '../assets/slack.png'
 function Ourpartners() {
   return (
     <>
-    <div className='bg-orange max-w-[1240px] mx-auto mt-40 mb-40 p-40'>
-      <h1 className='text-center text-white font-bold text-6xl'> Our Partners</h1>
-      <div className='grid grid-cols-1 lg:grid-cols-3  gap-x-8 gap-y-16  pt-12 sm:pt-20 '>
+    <div className=' max-w-[1240px] mx-auto mt-10 mb-40 p-40'>
+      <h1 className='text-center text-orange font-bold text-6xl'> Our Partners</h1>
+      <div className='grid grid-cols-1 lg:grid-cols-4  gap-x-8 gap-y-16  pt-12 sm:pt-20 '>
     <div className=''>
-    <h1>testing</h1>
+    <img src={google}  alt='/'/>
        </div>
        <div className=''>
-        <h1>testing</h1>
+       <img src={dropbox}  alt='/'/>
        
        </div>
        <div className=''>
-       <h1>testing</h1>
+       <img src={shopify}  alt='/'/>
        </div>
+       <div className=''>
+       <img src={slack}  alt='/'/>
+       </div>
+
+       
        
     </div>
       </div>
